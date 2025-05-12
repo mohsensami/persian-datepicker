40d4e59c434c48cd57c21018511c5e69941be16f
mohsensami <mohsensami18727@gmail.com>
2025-05-12 20:38:16 +0330

    update vite.config 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "index.ts"),
+      entry: path.resolve(__dirname, "index.ts"),
       name: "persian-datepicker",
-      fileName: (format) => `persian-datepicker.${format}.js`,
-      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ["react", "react-dom"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-          "react/jsx-runtime": "jsxRuntime",
-        },
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "style.css") return "persian-datepicker.css";
-          return assetInfo.name || "asset";
         },
       },
     },
